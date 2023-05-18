7cc764191114f73b4ac1d26b10f4ea19c99d57fd
MacksLH <99483061+MacksLH@users.noreply.github.com>
2023-05-18 10:56:44 +0200

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -3,10 +3,10 @@ from gensim.models import Word2Vec
 import numpy as np
 
 # Step 1: Load the Excel file into a DataFrame
-df = pd.read_excel('bajs2.xlsx')
+df = pd.read_csv('HelpdeskMjukvaru.csv')
 
 # Step 2: Preprocess the data and tokenize the sentences
-sentences = df['description'].tolist()
+sentences = df['Combined Words'].tolist()
 tokenized_sentences = [sentence.split() for sentence in sentences]
 
 # Step 3: Train the Word2Vec model
@@ -26,4 +26,4 @@ for i in range(len(sentence_embeddings[0])):
     df[f'embedding_{i+1}'] = [emb[i] if len(emb) > 0 else np.nan for emb in sentence_embeddings]
 
 # Step 6: Save the modified DataFrame to a new Excel file
-df.to_excel('word2vec_file2.xlsx', index=False)
+df.to_excel('HelpdeskMjukvaru_processed.xlsx', index=False)
