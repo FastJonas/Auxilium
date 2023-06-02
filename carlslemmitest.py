912d9fafdaa116a5f5caa79d0e297d29e7cc1729
Carl Kölqvist <132561117+CarlKolqvist@users.noreply.github.com>
2023-06-02 14:05:19 +0200

    Cleaning carlslemmitest.py
    UwU

--- carlslemmitest.py
+++ carlslemmitest.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import spacy
 
-# Load sv_core_news_sm
+# Smäll in sv_core_news_sm
 nlp = spacy.load('sv_core_news_sm')
 
 def lemmatize_text(text):
@@ -12,19 +12,17 @@ def lemmatize_text(text):
     lemmatized_text = ' '.join(lemmatized_words)
     return lemmatized_text
 
-# Read the File
 input_file = 'bajs3.xlsx'
 output_file = 'output.xlsx'
 
 df = pd.read_excel(input_file)
 
-# specify what columnm, in this case the description in column 8
+# description ligger i column 8
 text_column_index = 8
 
 # Lemmatizing
 df.iloc[:, text_column_index] = df.iloc[:, text_column_index].apply(lemmatize_text)
 
-# Write the updated data to a new file
 df.to_excel(output_file, index=False)
 
 print("Färdigt :D")
