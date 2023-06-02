dc7b89a42291fe2efac442dbde6ac7a0812120a6
edvinhellstrom <99483210+edvinhellstrom@users.noreply.github.com>
2023-06-02 18:23:09 +0200

    Add files via upload

---
+++ FinalStopwordRemover.py
@@ -0,0 +1,21 @@
+import nltk
+import pandas as pd
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+
+def remove_swedish_stopwords(text):
+    words = nltk.word_tokenize(text, language='swedish')
+    swedish_stopwords = set(stopwords.words('swedish'))
+    filtered_words = [word for word in words if word.lower() not in swedish_stopwords]
+    filtered_text = ' '.join(filtered_words)
+    
+    return filtered_text
+
+df = pd.read_excel('250PerTitle.xlsx')
+
+column_index = 1  
+
+df.iloc[:, column_index] = df.iloc[:, column_index].apply(remove_swedish_stopwords)
+
+df.to_excel('250PerTitleWithoutStopwords.xlsx', index=False)
