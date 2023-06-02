ac95206f04e0e4f559cbef0ad1114595e5d7b09a
Carl Kölqvist <132561117+CarlKolqvist@users.noreply.github.com>
2023-06-02 14:02:54 +0200

    cleaning Gör om occupation.py
    hello, roomservice

--- Gör om occupation.py
+++ Gör om occupation.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
 input_file = 'scaled_resultat.csv'
-
-# Read the CSV file into a pandas DataFrame
 df = pd.read_csv(input_file)
 
 # Extract the 'label' string from the column and overwrite the column with it
 df['Value'] = df['Value'].apply(lambda x: eval(x)['label'])
 
-# Save the modified DataFrame back to the original CSV file
 df.to_csv(input_file, index=False)
