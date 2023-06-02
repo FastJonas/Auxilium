e2a17b0ad7742f386a5aa94ad5eaf8beaeb3fad6
MacksLH <99483061+MacksLH@users.noreply.github.com>
2023-06-02 18:03:43 +0200

    Update Gör om occupation.py

--- Gör om occupation.py
+++ Gör om occupation.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-input_file = 'scaled_resultat.csv'
+input_file = 'resultAndScore_scaled.csv'
+
+# Read the CSV file into a pandas DataFrame
 df = pd.read_csv(input_file)
 
 # Extract the 'label' string from the column and overwrite the column with it
-df['Value'] = df['Value'].apply(lambda x: eval(x)['label'])
+df['Job Title'] = df['Job Title'].apply(lambda x: eval(x)['label'])
 
+# Save the modified DataFrame back to the original CSV file
 df.to_csv(input_file, index=False)
